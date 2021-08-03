ef456913f7ece8174b02b38b4130482be3b330fd
Aleksander Heintz <alxandr@alxandr.me>
2021-08-03 17:51:14 +0200

    Scaffold enet addon

---
+++ enet/rootfs/etc/cont-init.d/auth.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Setup auth data
+# ==============================================================================
+declare homeassistant_pw
+declare enet_pw
+
+AUTH_FILE=/data/auth.db
+
+if bashio::fs.file_exists ${AUTH_FILE}; then
+  bashio::log.info "Auth database exists"
+  bashio::exit.ok
+fi
+
+homeassistant_pw="$(pwgen 64 1)"
+enet_pw="$(pwgen 64 1)"
+
+bashio::log.info "Setup mqtt auth db"
+
+(
+  echo "enet:${enet_pw}"
+  echo "homeassistant:${homeassistant_pw}"
+) >"${AUTH_FILE}"
+
+# Encrypt data
+mosquitto_passwd -U ${AUTH_FILE}
+
+config=$(
+  bashio::var.json \
+    enet_password "${enet_pw}" \
+    homeassistant_password "${homeassistant_pw}"
+)
+
+echo "$config" >/data/auth.json
